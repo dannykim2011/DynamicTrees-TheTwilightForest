e326ead1218672621bbb89668499946bc8486a7b
dannykim2011 <danny4257@naver.com>
2026-06-26 16:18:38 +0900

    update

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        maven("https://maven.neoforged.net/releases")
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "DT Twilight Forest"
